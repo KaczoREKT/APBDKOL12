449e53ecfc585e1108a849968cf2cd022dc193a3
s28876 <kaczorekt6@gmail.com>
2024-06-28 13:13:29 +0200

    Commit2

---
+++ KOL1APP/KOL1APP/Program.cs
@@ -0,0 +1,27 @@
+using KOL1APP.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add your repositories and services
+builder.Services.AddScoped<IClientsRepository, ClientsRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
